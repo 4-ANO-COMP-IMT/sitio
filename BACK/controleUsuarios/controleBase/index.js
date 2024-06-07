7c71d5569bc4c166954c613069beda69b4bca6ba
bblopes <bblopeess@gmail.com>
2024-06-07 17:23:23 -0300

    Add Back Front

---
+++ BACK/controleUsuarios/controleBase/index.js
@@ -0,0 +1,63 @@
+const express = require('express');
+
+const fs = require('fs');
+const path = require('path');
+
+const app = express();
+app.use(express.json());
+
+
+let baseConsulta = {}
+
+// function cloneBaseUsuarios() {
+//     let baseConsulta = {}
+//     try {
+//         const data = fs.readFileSync(filePath, 'utf8');
+//         baseConsulta = JSON.parse(data);
+//         return baseConsulta
+//     } catch (error) {
+//         console.error('Erro ao carregar dados do arquivo:', error.message);
+//         return {}
+//     }
+// }
+function atualizarBaseUsuarios() {
+    try {
+        // Replace existing data with the contents of baseConsulta
+        fs.writeFileSync(filePath, JSON.stringify(baseConsulta, null, 2));
+        console.log('Dados atualizados e salvos com sucesso!');
+    } catch (error) {
+        console.error('Erro ao salvar dados no arquivo:', error.message);
+    }
+}
+
+const filePath = path.join(__dirname, '..', 'baseUsuarios.json'); // Caminho para o arquivo
+
+
+const funcoes = {
+
+    UsuarioCriado: (dataUsuario) => {
+        // let baseConsulta = cloneBaseUsuarios()
+        baseConsulta[Object.keys(baseConsulta).length] = dataUsuario;
+        atualizarBaseUsuarios()
+    },
+    ObservacaoCriada: (observacao) => {
+        const observacoes = baseConsulta[observacao.lembreteId]["observacoes"] || [];
+        observacoes.push(observacao);
+        baseConsulta[observacao.lembreteId]["observacoes"] = observacoes;
+    }
+    
+}
+
+app.get("/controleBase", (req, res) => {
+    res.status(200).send(baseConsulta);
+});
+
+app.post("/eventos", (req, res) => {
+    
+    funcoes[req.body.tipo](req.body.dados);
+    res.status(200).send(baseConsulta)
+    console.log(baseConsulta)
+
+});
+
+app.listen(6000, () => console.log("Consultas. Porta 6000"))
---
+++ BACK/controleUsuarios/criarUsuarios/index.js
@@ -0,0 +1,59 @@
+const express = require ('express');
+
+const axios = require('axios');
+const bodyParser = require('body-parser');
+const app = express();
+app.use(bodyParser.json());
+contador = 0;
+
+
+let baseLocal = {};
+
+
+app.get('/criarUsuarios', (req,res) => {
+    res.send(baseLocal)
+});
+
+app.put("/criarUsuarios", async (req, res) => {
+    // const baseLocal = {}
+
+    contador ++;
+    const { id } = req.body;
+    const { nome } = req.body;
+    const { email } = req.body;
+    const { senha } = req.body;
+
+
+    //save local
+    baseLocal[contador] = {
+        id,
+        nome,
+        email,
+        senha
+    };
+
+    
+    //POST mudanca na base de usuarios
+    //==========================================================
+    await axios.post("http://localhost:10000/eventos", {
+        tipo: "UsuarioCriado",
+        dados: {
+            id,
+            nome,
+            email,
+            senha
+        },
+    });
+    //==========================================================
+
+    res.status(201).send(baseLocal[contador]);
+})
+
+// app.post("/eventos", (req,res) => {
+//     console.log(req.body);
+//     res.status(200).send({msg:"ok"});
+// })
+
+app.listen(4000, () => {
+    console.log('atualizarBaseUsuarios. Porta 4000')
+})
