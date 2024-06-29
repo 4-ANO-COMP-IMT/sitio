72fc03e055129d7d9b8f3fff5820c884efb6b679
Pedro Mesquita <pedro.gjmesquita@gmail.com>
2024-06-29 17:02:18 -0300

    CORS adicionado

--- BACK/barramentoEventos/index.js
+++ BACK/barramentoEventos/index.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-
+const cors = require('cors');
 const axios = require('axios');
 
 const app = express();
 app.use(bodyParser.json());
+app.use(cors());
 
 function sleep(ms) {
     return new Promise(resolve => setTimeout(resolve, ms));
