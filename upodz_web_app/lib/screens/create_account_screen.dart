9a1df0666e5c51cfb17f38a8201449f169184c3e
Pedro Mesquita <pedro.gjmesquita@gmail.com>
2024-11-07 23:47:44 -0300

    Implement form validation in CreateAccountScreen; add email and phone number validators

--- upodz_web_app/lib/screens/create_account_screen.dart
+++ upodz_web_app/lib/screens/create_account_screen.dart
@@ -7,10 +7,15 @@ class CreateAccountScreen extends StatelessWidget {
   final TextEditingController _phoneController = TextEditingController();
   final TextEditingController _emailController = TextEditingController();
   final TextEditingController _passwordController = TextEditingController();
+  final _formKey = GlobalKey<FormState>();
 
   CreateAccountScreen({super.key});
 
   Future<void> _createUser(BuildContext context) async {
+    if (!_formKey.currentState!.validate()) {
+      return; // Retorna se o formulário não estiver válido
+    }
+
     final String name = _nameController.text;
     final String phone = _phoneController.text;
     final String email = _emailController.text;
@@ -34,14 +39,43 @@ class CreateAccountScreen extends StatelessWidget {
         Navigator.pushNamed(context, '/'); // Navega para a HomeScreen
       } else {
         ScaffoldMessenger.of(context).showSnackBar(
-          SnackBar(content: Text('Erro ao criar conta')),
+          const SnackBar(content: Text('Erro ao criar conta')),
         );
       }
     } catch (e) {
       ScaffoldMessenger.of(context).showSnackBar(
-        SnackBar(content: Text('Erro de conexão com o servidor')),
+        const SnackBar(content: Text('Erro de conexão com o servidor')),
       );
     }
+  }
+
+  String? _validateEmail(String? value) {
+    if (value == null || value.isEmpty) {
+      return 'Por favor, insira seu email';
+    }
+    final RegExp emailRegExp = RegExp(r'^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$');
+    if (!emailRegExp.hasMatch(value)) {
+      return 'Insira um email válido';
+    }
+    return null;
+  }
+
+  String? _validatePhone(String? value) {
+    if (value == null || value.isEmpty) {
+      return 'Por favor, insira seu telefone';
+    }
+    final RegExp phoneRegExp = RegExp(r'^[0-9]+$');
+    if (!phoneRegExp.hasMatch(value)) {
+      return 'O telefone deve conter apenas números';
+    }
+    return null;
+  }
+
+  String? _validateNotEmpty(String? value, String fieldName) {
+    if (value == null || value.isEmpty) {
+      return 'Por favor, insira seu $fieldName';
+    }
+    return null;
   }
 
   @override
@@ -51,106 +85,117 @@ class CreateAccountScreen extends StatelessWidget {
       body: Center(
         child: Padding(
           padding: const EdgeInsets.all(16.0),
-          child: Container(
-            width: 700, // Largura máxima do container
-            padding: const EdgeInsets.all(16.0),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              crossAxisAlignment: CrossAxisAlignment.stretch,
-              children: <Widget>[
-                SizedBox(
-                  width: 256,
-                  height: 256,
-                  child: Image.asset('assets/UPodzz.png'),
+          child: SingleChildScrollView( // Torna a coluna rolável
+            child: Container(
+              width: 700, // Largura máxima do container
+              padding: const EdgeInsets.all(16.0),
+              child: Form(
+                key: _formKey,
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.stretch,
+                  children: <Widget>[
+                    SizedBox(
+                      width: 256,
+                      height: 256,
+                      child: Image.asset('assets/UPodzz.png'),
+                    ),
+                    TextFormField(
+                      controller: _nameController,
+                      decoration: const InputDecoration(
+                        labelText: 'Nome',
+                        border: OutlineInputBorder(),
+                        focusedBorder: OutlineInputBorder(
+                          borderSide: BorderSide(color: Color(0xFF033F58)),
+                        ),
+                      ),
+                      validator: (value) => _validateNotEmpty(value, 'nome'),
+                    ),
+                    const SizedBox(height: 16.0),
+                    TextFormField(
+                      controller: _phoneController,
+                      decoration: const InputDecoration(
+                        labelText: 'Celular',
+                        border: OutlineInputBorder(),
+                        focusedBorder: OutlineInputBorder(
+                          borderSide: BorderSide(color: Color(0xFF033F58)),
+                        ),
+                      ),
+                      keyboardType: TextInputType.phone,
+                      validator: _validatePhone,
+                    ),
+                    const SizedBox(height: 16.0),
+                    TextFormField(
+                      controller: _emailController,
+                      decoration: const InputDecoration(
+                        labelText: 'Email',
+                        border: OutlineInputBorder(),
+                        focusedBorder: OutlineInputBorder(
+                          borderSide: BorderSide(color: Color(0xFF033F58)),
+                        ),
+                      ),
+                      keyboardType: TextInputType.emailAddress,
+                      validator: _validateEmail,
+                    ),
+                    const SizedBox(height: 16.0),
+                    TextFormField(
+                      controller: _passwordController,
+                      decoration: const InputDecoration(
+                        labelText: 'Senha',
+                        border: OutlineInputBorder(),
+                        focusedBorder: OutlineInputBorder(
+                          borderSide: BorderSide(color: Color(0xFF033F58)),
+                        ),
+                      ),
+                      obscureText: true,
+                      validator: (value) => _validateNotEmpty(value, 'senha'),
+                    ),
+                    const SizedBox(height: 24.0),
+                    OutlinedButton.icon(
+                      onPressed: () {
+                        _createUser(context); // Chama a função para enviar os dados
+                      },
+                      style: OutlinedButton.styleFrom(
+                        side: const BorderSide(color: Color(0xFF033F58), width: 2.0),
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(8.0),
+                        ),
+                        padding: const EdgeInsets.symmetric(vertical: 12.0),
+                      ),
+                      icon: const Icon(Icons.person_add, color: Color(0xFF033F58)),
+                      label: const Text(
+                        'Criar Conta',
+                        style: TextStyle(
+                          fontSize: 16,
+                          fontWeight: FontWeight.bold,
+                          color: Color(0xFF033F58),
+                        ),
+                      ),
+                    ),
+                    const SizedBox(height: 8.0),
+                    OutlinedButton(
+                      onPressed: () {
+                        Navigator.pushNamed(context, '/login'); // Volta para a tela de login
+                      },
+                      style: OutlinedButton.styleFrom(
+                        side: const BorderSide(color: Color(0xFF033F58), width: 2.0),
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(8.0),
+                        ),
+                        padding: const EdgeInsets.symmetric(vertical: 12.0),
+                      ),
+                      child: const Text(
+                        'Voltar para o Login',
+                        style: TextStyle(
+                          fontSize: 16,
+                          fontWeight: FontWeight.bold,
+                          color: Color(0xFF033F58),
+                        ),
+                      ),
+                    ),
+                  ],
                 ),
-                TextField(
-                  controller: _nameController,
-                  decoration: const InputDecoration(
-                    labelText: 'Nome',
-                    border: OutlineInputBorder(),
-                    focusedBorder: OutlineInputBorder(
-                      borderSide: BorderSide(color: Color(0xFF033F58)),
-                    ),
-                  ),
-                ),
-                const SizedBox(height: 16.0),
-                TextField(
-                  controller: _phoneController,
-                  decoration: const InputDecoration(
-                    labelText: 'Celular',
-                    border: OutlineInputBorder(),
-                    focusedBorder: OutlineInputBorder(
-                      borderSide: BorderSide(color: Color(0xFF033F58)),
-                    ),
-                  ),
-                ),
-                const SizedBox(height: 16.0),
-                TextField(
-                  controller: _emailController,
-                  decoration: const InputDecoration(
-                    labelText: 'Email',
-                    border: OutlineInputBorder(),
-                    focusedBorder: OutlineInputBorder(
-                      borderSide: BorderSide(color: Color(0xFF033F58)),
-                    ),
-                  ),
-                ),
-                const SizedBox(height: 16.0),
-                TextField(
-                  controller: _passwordController,
-                  decoration: const InputDecoration(
-                    labelText: 'Senha',
-                    border: OutlineInputBorder(),
-                    focusedBorder: OutlineInputBorder(
-                      borderSide: BorderSide(color: Color(0xFF033F58)),
-                    ),
-                  ),
-                  obscureText: true,
-                ),
-                const SizedBox(height: 24.0),
-                OutlinedButton.icon(
-                  onPressed: () {
-                    _createUser(context); // Chama a função para enviar os dados
-                  },
-                  style: OutlinedButton.styleFrom(
-                    side: const BorderSide(color: Color(0xFF033F58), width: 2.0),
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(8.0),
-                    ),
-                    padding: const EdgeInsets.symmetric(vertical: 12.0),
-                  ),
-                  icon: const Icon(Icons.person_add, color: Color(0xFF033F58)),
-                  label: const Text(
-                    'Criar Conta',
-                    style: TextStyle(
-                      fontSize: 16,
-                      fontWeight: FontWeight.bold,
-                      color: Color(0xFF033F58),
-                    ),
-                  ),
-                ),
-                const SizedBox(height: 8.0),
-                OutlinedButton(
-                  onPressed: () {
-                    Navigator.pushNamed(context, '/login'); // Chama a função para enviar os dados
-                  },
-                  style: OutlinedButton.styleFrom(
-                    side: const BorderSide(color: Color(0xFF033F58), width: 2.0),
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(8.0),
-                    ),
-                    padding: const EdgeInsets.symmetric(vertical: 12.0),
-                  ),
-                  child: const Text(
-                    'Voltar para o Login',
-                    style: TextStyle(
-                      fontSize: 16,
-                      fontWeight: FontWeight.bold,
-                      color: Color(0xFF033F58),
-                    ),
-                  ),
-                ),
-              ],
+              ),
             ),
           ),
         ),
